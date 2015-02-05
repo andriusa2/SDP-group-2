9f65837d3f2df1c69a7d20695aafb637264ad566
Alex <konga.kearney@gmail.com>
2015-02-05 10:56:31 +0000

    added notes on the first attacker, which is the same as the second attacker

--- lib/strategy/attacker1.py
+++ lib/strategy/attacker1.py
@@ -1,1 +1,51 @@
 __author__ = 'alex'
+
+
+class attacker1:
+
+    def __init__(self):
+        pass
+
+    def act(self):
+        pass
+        # is the ball in our zone?
+
+        # yes: (the only option for this ms)
+
+        # are we facing the ball?
+
+            # no:
+
+            # turn towards the the ball
+
+            # yes:
+
+            # start moving towards the ball
+
+        # are we close enough to the ball?
+
+            # no:
+
+            # keep moving
+
+            # yes:
+
+            # stop and lower cage
+
+        # do we have the ball?
+
+            # no: (need to think more about this)
+
+            # yes:
+
+        # are we facing the goal?
+
+            # no:
+
+            # turn towards goal
+
+            # yes:
+
+            # kick ball
+
+        # Complete
