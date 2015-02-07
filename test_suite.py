7f8834a8899c992d7accc1662bf1513519b41610
Sam Davies <samdavies@live.co.uk>
2015-02-07 16:20:38 +0000

    tests made to ensure that the world state is fetched

--- test_suite.py
+++ test_suite.py
@@ -1,7 +1,7 @@
 _author__ = 'Sam Davies'
 import unittest
 from lib.world.world_state import Robot, Ball, WorldState
-from lib.strategy.attacker1 import attacker1
+from lib.strategy.attacker1 import Attacker1
 from numpy import pi
 
 
@@ -19,11 +19,23 @@ class TestWorldState(unittest.TestCase):
 class TestAttacker1(unittest.TestCase):
 
     def setUp(self):
-        pass
+        # create a robot and a ball
+        robot_1 = Robot(direction=pi, position=(8, 8), velocity=(0, 0), enemy=True)
+        ball = Ball(position=(10, 10), velocity=(0, 0), in_possession=False)
+
+        # set the list of robots
+        robots = dict(left_attacker=robot_1)
+
+        a_world_state = WorldState(robots=robots, ball=ball, zone_boundaries=None)
+        actual_robot = None
+        self.attacker1 = Attacker1(a_world_state, "left_attacker", actual_robot)
 
     # ensure that the world state is fetched
     def test_fetch_world_state(self):
-        pass
+        self.attacker1.fetch_world_state()
+        # check that the correct location is fetched for robot and ball
+        self.assertEquals((self.attacker1.robot_loc_x, self.attacker1.robot_loc_y) == (8, 8))
+        self.assertEquals((self.attacker1.ball_loc_x, self.attacker1.ball_loc_y) == (10, 10))
 
     # ensure that a close ball is found to be close
 
