a209a39f9fb299a985fcc240fe3d173f6f2209ad
Sam Davies <samdavies@live.co.uk>
2015-02-08 12:43:43 +0000

    tests for robots ability to turn

--- test_suite.py
+++ test_suite.py
@@ -2,6 +2,7 @@ _author__ = 'Sam Davies'
 import unittest
 from lib.world.world_state import Robot, Ball, WorldState, Zone
 from lib.strategy.attacker1 import Attacker1
+from communication.dummy_robot import DummyRobot
 from numpy import pi
 
 
@@ -29,8 +30,11 @@ class TestAttacker1(unittest.TestCase):
         # set the list of robots
         robots = [robot_1, robot_2, robot_3, robot_4]
 
+        # create a world object
         self.world_state = WorldState(robots=robots, ball=ball, zone_boundaries=None)
-        actual_robot = None
+        # make a dummy robot which can change the world
+        actual_robot = DummyRobot(self.world_state)
+        # give the strategy the world the dummy and the zone of the dummy
         self.attacker1 = Attacker1(self.world_state, Zone.L_ATT, actual_robot)
 
     # ensure that the world state is fetched
@@ -57,6 +61,23 @@ class TestAttacker1(unittest.TestCase):
         self.change_ball_location(23, 23)
         # check that the ball is close
         self.assertFalse(self.attacker1.is_ball_close())
+
+    # ensure that a robot will turn towards the ball
+    def test_robot_turns_towards_ball(self):
+        # set up the world so that the robot has to turn
+        ball = Ball(position=(10.0, 10.0), velocity=(0, 0), in_possession=False)
+        robot_2 = Robot(direction=pi, position=(20, 20), velocity=(0, 0), enemy=True)
+        self.world_state.set_ball(ball)
+        self.world_state.add_robot(Zone.L_ATT, robot_2)
+        #
+        self.attacker1.act()
+        # check that robot if facing the ball
+        self.assertTrue(self.attacker1.is_robot_facing_ball())
+
+    # ensure that a robot won't turn if ball already in line
+
+    # ensure the the robot will lower cage on a close ball
+
 
 if __name__ == '__main__':
     unittest.main()
