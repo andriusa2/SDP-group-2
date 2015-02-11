3809d5583a2c8d40f9d4891a218d9a50fb6b675f
Sam Davies <samdavies@live.co.uk>
2015-02-11 19:29:03 +0000

    separated the vision controller from the main controller

--- controller.py
+++ controller.py
@@ -1,165 +1,71 @@
-from vision.vision import Vision, Camera, GUI
-from postprocessing.postprocessing import Postprocessing
-from preprocessing.preprocessing import Preprocessing
-import vision.tools as tools
-from cv2 import waitKey
-import cv2
-import serial
-import warnings
-import time
+from lib.strategy.planner import Planner
+from lib.world.world_state import WorldState, Zone, Robot, Ball
+from vision.vision_controller import VisionController
+
+__name__ = "Sam Davies"
 
 
-warnings.filterwarnings("ignore", category=DeprecationWarning)
+class Controller(object):
 
+    def __init__(self, pitch, color, our_side):
 
-class Controller:
-    """
-    Primary source of robot control. Ties vision and planning together.
-    """
+        boundaries = [47, 106, 165, 212]
+        self.world = WorldState()
+        actual_robot = Controller("/dev/ttyACM0")
 
-    def __init__(self, pitch, color, our_side, video_port=0, comm_port='/dev/ttyUSB0', comms=1):
+        # set the boundaries
+        self.world.set_zone_boundaries(boundaries)
+
+        # this is our chosen strategy
+        """R_ATT, L_ATT, R_DEF, L_DEF"""
+        self.planner = Planner(self.world, Zone.L_ATT, actual_robot, True)
+
+        self.vision = VisionController(pitch, color, our_side, comms=0)
+
+    def main(self):
         """
-        Entry point for the SDP system.
-
-        Params:
-            [int] video_port                port number for the camera
-            [string] comm_port              port number for the arduino
-            [int] pitch                     0 - main pitch, 1 - secondary pitch
-            [string] our_side               the side we're on - 'left' or 'right'
-            *[int] port                     The camera port to take the feed from
-            *[Robot_Controller] attacker    Robot controller object - Attacker Robot has a RED
-                                            power wire
-            *[Robot_Controller] defender    Robot controller object - Defender Robot has a YELLOW
-                                            power wire
+        grabs the latest frame from vision, update the
+        world based on that frame and runs the next plan
+        :return: nothing
         """
-        assert pitch in [0, 1]
-        assert color in ['yellow', 'blue']
-        assert our_side in ['left', 'right']
+        # let the vision update the world
+        self.vision.send_model_to_planner(self.update_world_state)
 
-        self.pitch = pitch
+        # do the next plan
+        self.planner.plan_attack()
 
-        # Set up the Arduino communications
-        # self.arduino = Arduino(comm_port, 115200, 1, comms)
-
-        # Set up camera for frames
-        self.camera = Camera(port=video_port, pitch=self.pitch)
-        frame = self.camera.get_frame()
-        center_point = self.camera.get_adjusted_center(frame)
-
-        # Set up vision
-        self.calibration = tools.get_colors(pitch)
-        self.vision = Vision(
-            pitch=pitch, color=color, our_side=our_side,
-            frame_shape=frame.shape, frame_center=center_point,
-            calibration=self.calibration)
-
-        # Set up postprocessing for vision
-        self.postprocessing = Postprocessing()
-
-        # Set up main planner
-        # self.planner = Planner(our_side=our_side, pitch_num=self.pitch)
-
-        # Set up GUI
-        # self.GUI = GUI(calibration=self.calibration, arduino=self.arduino, pitch=self.pitch)
-        self.GUI = GUI(calibration=self.calibration, arduino=None, pitch=self.pitch)
-
-        self.color = color
-        self.side = our_side
-
-        self.preprocessing = Preprocessing()
-
-        # self.attacker = Attacker_Controller()
-        # self.defender = Defender_Controller()
-
-    def wow(self):
+    def fetch_our_zone(self, zone_num):
         """
-        Ready your sword, here be dragons.
+        choose are zone starting with 1 being at the left and 4 being at the right
+        :param zone_num
         """
-        counter = 1L
-        timer = time.clock()
-        try:
-            c = True
-            while c != 27:  # the ESC key
+        assert zone_num in [1, 2, 3, 4]
 
-                frame = self.camera.get_frame()
-                pre_options = self.preprocessing.options
-                # Apply preprocessing methods toggled in the UI
-                preprocessed = self.preprocessing.run(frame, pre_options)
-                frame = preprocessed['frame']
-                if 'background_sub' in preprocessed:
-                    cv2.imshow('bg sub', preprocessed['background_sub'])
-                # Find object positions
-                # model_positions have their y coordinate inverted
-
-                model_positions, regular_positions = self.vision.locate(frame)
-                model_positions = self.postprocessing.analyze(model_positions)
-
-                # Find appropriate action
-                # self.planner.update_world(model_positions)
-                # attacker_actions = self.planner.plan('attacker')
-                # defender_actions = self.planner.plan('defender')
-
-                # if self.attacker is not None:
-                    # self.attacker.execute(self.arduino, attacker_actions)
-                # if self.defender is not None:
-                    # self.defender.execute(self.arduino, defender_actions)
-
-                # Information about the grabbers from the world
-                # grabbers = {
-                    # 'our_defender': self.planner._world.our_defender.catcher_area,
-                    # 'our_attacker': self.planner._world.our_attacker.catcher_area
-                # }
-
-                # Information about states
-                # attackerState = (self.planner.attacker_state, self.planner.attacker_strat_state)
-                # defenderState = (self.planner.defender_state, self.planner.defender_strat_state)
-
-                # Use 'y', 'b', 'r' to change color.
-                c = waitKey(2) & 0xFF
-                actions = []
-                fps = float(counter) / (time.clock() - timer)
-                # Draw vision content and actions
-
-                # self.GUI.draw(
-                #     frame, model_positions, actions, regular_positions, fps, attackerState,
-                #     defenderState, attacker_actions, defender_actions, grabbers,
-                #     our_color=self.color, our_side=self.side, key=c, preprocess=pre_options)
-                self.GUI.draw(
-                    frame, model_positions, actions, regular_positions, fps, 
-                    our_color=self.color, our_side=self.side, key=c, preprocess=pre_options)
-                counter += 1
-
-        except:
-            # if self.defender is not None:
-            #     self.defender.shutdown(self.arduino)
-            # if self.attacker is not None:
-            #     self.attacker.shutdown(self.arduino)
-            raise
-
-        finally:
-            # Write the new calibrations to a file.
-            tools.save_colors(self.pitch, self.calibration)
-            # if self.attacker is not None:
-            #     self.attacker.shutdown(self.arduino)
-            # if self.defender is not None:
-            #     self.defender.shutdown(self.arduino)
-
-
-class Robot_Controller(object):
-    """
-    Robot_Controller superclass for robot control.
-    """
-
-    def __init__(self):
+    def update_world_state(self, model_positions):
         """
-        Connect to Brick and setup Motors/Sensors.
+        Change the state of the world based on the current frame
+        :param model_positions: the values to change to world to
+        :return: none
         """
-        self.current_speed = 0
+        scale_factor = 0.4
 
-    def shutdown(self, comm):
-        # TO DO
-            pass
+        robot_models = [model_positions['our_defender'], model_positions['our_attacker'],
+                        model_positions['their_defender'], model_positions['their_attacker']]
 
+        robot1 = Robot().convert_from_model(robot_models[0], scale_factor)
+        robot2 = Robot().convert_from_model(robot_models[1], scale_factor)
+        robot3 = Robot().convert_from_model(robot_models[2], scale_factor)
+        robot4 = Robot().convert_from_model(robot_models[3], scale_factor)
+
+        robots = [robot1, robot2, robot3, robot4]
+
+        model_ball = model_positions['ball']
+        ball = Ball().convert_from_model(model_ball, scale_factor)
+
+        # change the states of the robots in the world
+        self.world.set_robots_list(robots)
+        # change the position of the ball
+        self.world.set_ball(ball)
 
 if __name__ == '__main__':
     import argparse
@@ -167,13 +73,7 @@ if __name__ == '__main__':
     parser.add_argument("pitch", help="[0] Main pitch, [1] Secondary pitch")
     parser.add_argument("side", help="The side of our defender ['left', 'right'] allowed.")
     parser.add_argument("color", help="The color of our team - ['yellow', 'blue'] allowed.")
-    parser.add_argument(
-        "-n", "--nocomms", help="Disables sending commands to the robot.", action="store_true")
 
     args = parser.parse_args()
-    if args.nocomms:
-        c = Controller(
-            pitch=int(args.pitch), color=args.color, our_side=args.side, comms=0).wow()
-    else:
-        c = Controller(
-            pitch=int(args.pitch), color=args.color, our_side=args.side).wow()
+    c = Controller(pitch=int(args.pitch), color=args.color, our_side=args.side)
+    c.main()
--- vision/vision_controller.py
+++ vision/vision_controller.py
@@ -7,11 +7,6 @@ import cv2
 import warnings
 import time
 
-from communication.controller import Controller
-from communication.dummy_robot import DummyRobot
-from lib.strategy.planner import Planner
-from lib.world.world_state import WorldState, Zone, Robot, Ball
-
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
@@ -59,23 +54,7 @@ class VisionController:
 
         self.preprocessing = Preprocessing()
 
-        #---------------------- PLANNER ---------------------------
-        boundries = [47, 106, 165, 212]
-        self.world = WorldState()
-        actual_robot = Controller("/dev/ttyACM0")
-
-        # set the initial state
-        # self.update_world_state()
-        # set the boundries
-        self.world.set_zone_boundaries(boundries)
-
-        # this is our chosen strategy
-        """R_ATT, L_ATT, R_DEF, L_DEF"""
-        self.planner = Planner(self.world, Zone.L_ATT, actual_robot, True)
-
-        #----------------------------------------------------------
-
-    def wow(self):
+    def send_model_to_planner(self, planning_function):
         """
         Ready your sword, here be dragons.
         """
@@ -99,8 +78,7 @@ class VisionController:
                 model_positions = self.postprocessing.analyze(model_positions)
 
                 #---------------------- PLANNER ---------------------------
-                self.update_world_state(model_positions)
-                self.planner.plan_attack()
+                planning_function(model_positions)
                 #----------------------------------------------------------
 
                 # Use 'y', 'b', 'r' to change color.
@@ -120,45 +98,3 @@ class VisionController:
             # Write the new calibrations to a file.
             tools.save_colors(self.pitch, self.calibration)
 
-    def update_world_state(self, model_positions):
-        """
-        Change the state of the world based on the current frame
-        :param model_positions: the values to change to world to
-        :return: none
-        """
-        scale_factor = 0.4
-
-        robot_models = [model_positions['our_defender'], model_positions['our_attacker'],
-                        model_positions['their_defender'], model_positions['their_attacker']]
-
-        robot1 = Robot().convert_from_model(robot_models[0], scale_factor)
-        robot2 = Robot().convert_from_model(robot_models[1], scale_factor)
-        robot3 = Robot().convert_from_model(robot_models[2], scale_factor)
-        robot4 = Robot().convert_from_model(robot_models[3], scale_factor)
-
-        robots = [robot1, robot2, robot3, robot4]
-
-        model_ball = model_positions['ball']
-        ball = Ball().convert_from_model(model_ball, scale_factor)
-
-        # change the states of the robots in the world
-        self.world.set_robots_list(robots)
-        # change the position of the ball
-        self.world.set_ball(ball)
-
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("pitch", help="[0] Main pitch, [1] Secondary pitch")
-    parser.add_argument("side", help="The side of our defender ['left', 'right'] allowed.")
-    parser.add_argument("color", help="The color of our team - ['yellow', 'blue'] allowed.")
-    parser.add_argument(
-        "-n", "--nocomms", help="Disables sending commands to the robot.", action="store_true")
-
-    args = parser.parse_args()
-    if args.nocomms:
-        c = VisionController(
-            pitch=int(args.pitch), color=args.color, our_side=args.side, comms=0).wow()
-    else:
-        c = VisionController(
-            pitch=int(args.pitch), color=args.color, our_side=args.side).wow()
